e7b2a888aeb4d8a6a1d7a822d9c449f862d3766f
mini0n <mini0n.000@gmail.com>
2025-08-12 06:37:44 -0600

    updates

---
+++ nvim/lua/plugins/hlchunk.lua
@@ -0,0 +1,35 @@
+
+return {
+	"shellRaining/hlchunk.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+			require("hlchunk").setup({
+				chunk = {
+					enable = true,
+					use_treesitter = true,
+					style = {
+            { fg = "#6d5d89" }, -- Normal chunk color
+						{ fg = "#c21f30" }, -- Error chunk color
+					},
+					chars = {
+						horizontal_line = "─",
+						vertical_line = "│",
+						left_top = "╭",
+						left_bottom = "╰",
+						right_arrow = "─",
+					},
+					error_sign = true,
+          -- Animation settings
+          duration = 0, -- Animation duration in seconds
+          delay = 0, -- Delay before the animation starts
+				},
+				line_num = {
+					enable = true,
+          style = "#806d9c",
+				},
+        indent = {
+          enable = true,
+        },
+			})
+	end
+}
--- nvim/lua/plugins/indent-blankline.lua
+++ nvim/lua/plugins/indent-blankline.lua
@@ -1,11 +1,11 @@
 return {
-  "lukas-reineke/indent-blankline.nvim",
-  main = "ibl",
-  opts = {},
-  config = function()
-    require("ibl").setup({
-      scope = { enabled = false },
-      indent = { char = "|" },
-    })
-  end
+  -- "lukas-reineke/indent-blankline.nvim",
+  -- main = "ibl",
+  -- opts = {},
+  -- config = function()
+  --   require("ibl").setup({
+  --     scope = { enabled = false },
+  --     indent = { char = "|" },
+  --   })
+  -- end
 }
