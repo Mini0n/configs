09daeb7e982aee2536ffce4641a70ed550c96ae0
mini0n <mini0n.000@gmail.com>
2026-01-11 19:52:49 -0600

    Updated rubocop kaymap cmd

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -6,8 +6,19 @@
 vim.keymap.set("n", "<leader>ii", vim.diagnostic.open_float, { desc = "Show diagnostics" })
 -- vim.keymap.set("n", "<leader>ia", vim.diagnostic.open_float({scope="buffer"}), { desc = "Show buffer diagnostics" })
 
--- rubocop autocorrect
+-- rubocop autocorrect (LSP-aware)
 vim.keymap.set('n', '<leader>ru', function()
-  vim.cmd('w')              -- Save the current buffer
-  vim.cmd('!rubocop -A %')  -- Run RuboCop autocorrect on the current file (%)
-end, { desc = 'RuboCop Autocorrect' })
+  vim.lsp.buf.code_action({
+    context = { only = { 'source.fixAll.rubocop' } },
+    apply = true
+  })
+end, { desc = 'RuboCop LSP Autocorrect' })
+
+-- Alternative: run rubocop autocorrect on save
+-- vim.api.nvim_create_autocmd("BufWritePre", {
+  -- pattern = "*.rb",
+  -- callback = function()
+    -- Uncomment the next line if you want automatic rubocop fixes on save
+    -- vim.lsp.buf.code_action({ context = { only = { 'source.fixAll.rubocop' } }, apply = true })
+  -- end,
+-- })
